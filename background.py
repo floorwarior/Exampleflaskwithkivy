25890c68b31348a4195945455a9ea8b056b52faf
floorwarior <145608003+floorwarior@users.noreply.github.com>
2025-02-11 22:24:14 +0100

    Update background.py

--- background.py
+++ background.py
@@ -2,16 +2,14 @@
 from flask import Flask
 from flask import render_template
 
-
+PythonService = autoclass("org.kivy.android.PythonService")
+PythonService.mService.setAutoRestartService(True)
 
 app = Flask(__name__)
 
 @app.route("/")
 def hellothere():
 	return render_template("generalkenobi.html")
-
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
